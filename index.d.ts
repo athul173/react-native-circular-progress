d345ce8fa66bdefca20d09f19d9bdd68d869b64c
athul173 <athulakhil173@gmail.com>
2023-01-18 21:57:49 +0100

    refactor: Updates types - single tintColors props
    Removes tintColorSecondary  and colors prop types in favour of a
    tintColors prop that can be either an array of transition colors or a
    single color for progress

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,12 @@
-declare module 'react-native-circular-progress' {
-  import * as React from 'react';
+declare module "react-native-circular-progress" {
+  import * as React from "react";
   import {
     Animated,
     Easing,
     EasingFunction,
     StyleProp,
-    ViewStyle
-  } from 'react-native';
-  import { ViewPropTypes } from 'deprecated-react-native-prop-types';
+    ViewStyle,
+  } from "react-native";
 
   export interface AnimatedCircularProgressProps {
     /**
@@ -40,28 +39,12 @@ declare module 'react-native-circular-progress' {
     backgroundWidth?: number;
 
     /**
-     * Color of the progress line
+     * Color of the progress line or an array of colors that progresses proportionally from 0 to 100
      *
      * @type {string}
      * @default 'black'
      */
-    tintColor?: string;
-
-    /**
-     * Change the fill color from tintColor to tintColorSecondary as animation progresses.
-     *
-     * @type {string}
-     * @default 'undefined'
-     */
-    tintColorSecondary?: string;
-    
-    /**
-     * Custom color array for the progress animation
-     *      
-     * @type {string[]}
-     * @default 'undefined'
-     */
-    colors?: string[];
+    tintColors?: string | string[];
 
     /**
      * Current progress / tint transparency
@@ -92,7 +75,7 @@ declare module 'react-native-circular-progress' {
      * @type {('butt' | 'round' | 'square')}
      * @default 'butt'
      */
-    lineCap?: 'butt' | 'round' | 'square';
+    lineCap?: "butt" | "round" | "square";
 
     /**
      * Shape used at ends of progress line.
@@ -100,7 +83,7 @@ declare module 'react-native-circular-progress' {
      * @type {('butt' | 'round' | 'square')}
      * @default lineCap - which is 'butt'
      */
-    fillLineCap?: 'butt' | 'round' | 'square';
+    fillLineCap?: "butt" | "round" | "square";
 
     /**
      * If you don't want a full circle, specify the arc angle
@@ -203,9 +186,7 @@ declare module 'react-native-circular-progress' {
     dashedBackground?: { width: number; gap: number };
   }
 
-  export class AnimatedCircularProgress extends React.Component<
-    AnimatedCircularProgressProps
-  > {
+  export class AnimatedCircularProgress extends React.Component<AnimatedCircularProgressProps> {
     /**
      * Animate the progress bar to a specific value
      *
@@ -213,7 +194,11 @@ declare module 'react-native-circular-progress' {
      * @param {number} duration
      * @param {Function} ease
      */
-    animate: (toVal: number, duration: number, ease?: EasingFunction) => Animated.CompositeAnimation;
+    animate: (
+      toVal: number,
+      duration: number,
+      ease?: EasingFunction
+    ) => Animated.CompositeAnimation;
 
     /**
      * Re-run animation with a specified prefill-value
